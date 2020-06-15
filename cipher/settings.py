aea485f0e54ab139e16ee7843e5f2edb468f36db
Tushar <tushar.mohan2001@gmail.com>
2020-06-16 01:35:13 +0530

    static root added

--- cipher/settings.py
+++ cipher/settings.py
@@ -123,17 +123,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+    os.path.join(PROJECT_ROOT,'static')
 ]
 MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_ROOT=os.path.join(PROJECT_ROOT,'media')
 
 LOGIN_REDIRECT_URL='main/'
 LOGIN_URL='/main/login'
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
