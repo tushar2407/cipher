8eb0b26240058ef027921e301228068f5eaeff03
Tushar <tushar.mohan2001@gmail.com>
2020-06-16 14:52:12 +0530

    updated services

--- services/encrypt.py
+++ services/encrypt.py
@@ -15,6 +15,7 @@ def encrypt(content):
         return s
 def encrypt1(file_in, password, file_out):
     #file_in='C:\\Users\\Tushar\\projects\\django\\cipher\\cipher'+file_in
+    file_in='/app'+file_in
     document_in = PdfFileReader(open(file_in, 'rb'))
     document_out = PdfFileWriter()
     document_out.cloneReaderDocumentRoot(document_in)
