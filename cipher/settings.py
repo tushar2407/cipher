8b70d01c343012fb47a89bb22c03fe54b5aff0be
Tushar <tushar.mohan2001@gmail.com>
2020-06-16 14:24:19 +0530

    trying day2

--- cipher/settings.py
+++ cipher/settings.py
@@ -137,6 +137,6 @@ LOGIN_URL='/main/login'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-"""import dj_database_url 
+import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)"""
+DATABASES['default'].update(prod_db)
